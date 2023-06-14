d8b458761dbea7b91d30f9017412b1af945ed17b
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-06-13 23:10:24 -0600

    Adding the products api modifications

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,44 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+      include: [{
+        model: Category,
+        attributes: ['id', 'category_name']
+      }]
+})
+})
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }]
+    })
+    .then((product) => {
+      if (!product) {
+        res.status(404).json({
+          message: 'No product found with this id'
+        });
+        return;
+      }
+      res.json(product);
+    })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
